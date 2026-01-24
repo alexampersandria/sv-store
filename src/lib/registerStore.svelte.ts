832b222ac99c6d06ba04c3a1f7f23c8bea0d333c
LIARA <liarabruchmann@gmail.com>
2026-01-24 12:15:22 +0100

    perf: optimize registerStore

--- src/lib/registerStore.svelte.ts
+++ src/lib/registerStore.svelte.ts
@@ -23,27 +23,21 @@ export const registerStore = (
   store: any,
   options?: SvStoreOptions,
 ) => {
+  if (typeof window === 'undefined') return
+
   let mounted: boolean = false
 
-  let prefix = options?.prefix
-  if (prefix === undefined) prefix = 'sv-store'
-  const storeName = prefix ? `${prefix}:${name}` : name
+  const prefix =
+    options?.prefix === null ? null : (options?.prefix ?? 'sv-store')
+  const key = prefix ? `${prefix}:${name}` : name
+  const space =
+    options?.type === 'sessionStorage' ? sessionStorage : localStorage
 
-  const storeEffect = (updatedStore: any) => {
+  const storeEffect = (state: any) => {
     untrack(() => options?.beforeWrite?.(store))
 
-    const storeObject: { [key: string]: any } = {}
-    const keys = Object.keys(updatedStore)
-    keys.forEach(key => {
-      storeObject[key] = (updatedStore as { [key: string]: any })[key]
-    })
-    if (mounted) {
-      if (options?.type === 'localStorage' || !options?.type) {
-        localStorage.setItem(storeName, JSON.stringify(storeObject))
-      } else if (options?.type === 'sessionStorage') {
-        sessionStorage.setItem(storeName, JSON.stringify(storeObject))
-      }
-    }
+    const copy = { ...state }
+    if (mounted) space.setItem(key, JSON.stringify(copy))
 
     untrack(() => options?.afterWrite?.(store))
   }
@@ -51,25 +45,18 @@ export const registerStore = (
   const readStore = () => {
     untrack(() => options?.beforeRead?.(store))
 
-    let stored: string | null = null
-    if (options?.type === 'localStorage' || !options?.type) {
-      stored = localStorage.getItem(storeName)
-    } else if (options?.type === 'sessionStorage') {
-      stored = sessionStorage.getItem(storeName)
-    }
+    const stored = space.getItem(key)
 
     if (stored) {
-      const storedObject = JSON.parse(stored)
-      const keys = Object.keys(storedObject)
-      keys.forEach(key => {
-        if (store[key] !== storedObject[key]) {
+      const state = JSON.parse(stored)
+      for (const key in state) {
+        if (store[key] !== state[key]) {
           const properties = Object.getOwnPropertyDescriptor(store, key)
-          // if property has setter
           if (properties?.set) {
-            store[key] = storedObject[key]
+            store[key] = state[key]
           }
         }
-      })
+      }
     }
 
     untrack(() => options?.afterRead?.(store))
@@ -80,7 +67,5 @@ export const registerStore = (
     mounted = true
   })
 
-  $effect(() => {
-    storeEffect(store)
-  })
+  $effect(() => storeEffect(store))
 }
